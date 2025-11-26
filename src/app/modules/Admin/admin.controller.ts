1cfbd369485b3c05055dc3e06cff40f3eb69ca20
shariful10 <freelancershariful10@gmail.com>
2025-11-26 23:56:43 +0600

    feat(admin.service): implement query filtering in getAllAdminsFromDB and add pick utility function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,10 +1,17 @@
-import catchAsync from "../../utils/catchAsync";
+import pick from "../../shared/pick";
 import { AdminService } from "./admin.service";
+import catchAsync from "../../utils/catchAsync";
 import { httpStatus } from "../../utils/httpStatus";
 import sendResponse from "../../utils/sendResponse";
 
 const getAllAdmins = catchAsync(async (req, res) => {
-  const result = await AdminService.getAllAdminsFromDB(req.query);
+  const query = pick(req.query, [
+    "name",
+    "email",
+    "searchTerm",
+    "contactNumber",
+  ]);
+  const result = await AdminService.getAllAdminsFromDB(query);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
