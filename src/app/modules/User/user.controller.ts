aef14199bdbef3de09e1a9a5e16e2ec1ebd09313
shariful10 <freelancershariful10@gmail.com>
2025-11-26 11:24:17 +0600

    feat(admin): implement createAdmin functionality and update related services and routes

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -4,12 +4,13 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { httpStatus } from "../../utils/httpStatus";
 
-const createUser = catchAsync(async (req, res) => {
-  const result = await UserService.createUserIntoDB(req.body);
+const createAdmin = catchAsync(async (req, res) => {
+  const result = await UserService.createAdminIntoDB(req.body);
 
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
-    message: result.message,
+    message: "Admin created successfully!",
+    data: result,
   });
 });
 
@@ -64,7 +65,7 @@ const deleteUser = catchAsync(async (req, res) => {
 });
 
 export const UserController = {
-  createUser,
+  createAdmin,
   getAllUser,
   updateUser,
   deleteUser,
---
+++ src/app/modules/User/user.interface.ts
@@ -0,0 +1,8 @@
+export type AdminPayload = {
+  admin: {
+    name: string;
+    email: string;
+    contactNumber: string;
+  };
+  password: string;
+};
