d99f2ba946b0d5cf4305ae12eaeec52ef6b26aa6
shariful10 <freelancershariful10@gmail.com>
2025-12-21 12:38:28 +0600

    feat(patient): implement CRUD operations and validations for patients

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from "../modules/Auth/auth.route";
 import { UserRoutes } from "../modules/User/user.routes";
 import { AdminRoutes } from "../modules/Admin/admin.route";
 import { DoctorRoutes } from "../modules/Doctor/doctor.route";
+import { PatientRoutes } from "../modules/Patient/patient.route";
 import { SpecialtyRoutes } from "../modules/Specialty/specialty.route";
 import { FileUploadRoutes } from "../modules/FileUpload/fileUpload.route";
 
@@ -24,6 +25,10 @@ const moduleRoutes = [
   {
     path: "/doctors",
     route: DoctorRoutes,
+  },
+  {
+    path: "/patients",
+    route: PatientRoutes,
   },
   {
     path: "/files",
