71f08f87567d15f57c24106a931cde57a56b3e55
shariful10 <freelancershariful10@gmail.com>
2026-01-11 23:38:34 +0600

    refactor(schedule): remove unused imports and enhance error message for schedule creation

--- src/app/modules/Schedule/schedule.controller.ts
+++ src/app/modules/Schedule/schedule.controller.ts
@@ -1,9 +1,7 @@
-import pick from "../../shared/pick";
 import catchAsync from "../../utils/catchAsync";
 import { httpStatus } from "../../utils/httpStatus";
 import sendResponse from "../../utils/sendResponse";
 import { ScheduleService } from "./schedule.service";
-import { metaFields } from "../../interface/metaFields";
 
 const createSchedule = catchAsync(async (req, res) => {
   const result = await ScheduleService.createScheduleInToDB(req.body);
