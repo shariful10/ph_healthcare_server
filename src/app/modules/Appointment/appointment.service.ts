33db6c79c1d76ca7aa10257974e99a359ace5564
shariful10 <freelancershariful10@gmail.com>
2026-06-13 22:58:39 +0600

    fix(appointments): correct month indexing in transactionId generation

--- src/app/modules/Appointment/appointment.service.ts
+++ src/app/modules/Appointment/appointment.service.ts
@@ -57,7 +57,7 @@ const createAppointmentInToDB = async (payload: Appointment, id: string) => {
 
     // PH-Healthcare-datetime: 2024-06-17T12:00:00.000Z
     const today = new Date();
-    const transactionId = `PH-HealthCare-${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}-${today.getHours()}-${today.getMinutes()}`;
+    const transactionId = `PH-HealthCare-${today.getFullYear()}-${today.getMonth()}-${today.getDate()}-${today.getHours()}-${today.getMinutes()}`;
 
     await tx.payment.create({
       data: {
